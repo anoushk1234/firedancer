325c1a8144d68b94a6020d0a659f0e2b55e7309b
Anoushk Kharangate <anoushk@tinydancer.io>
2025-05-03 22:06:16 +0530

    linker
    Signed-off-by: Anoushk Kharangate <anoushk@tinydancer.io>

--- config/extra/with-notcurses.mk
+++ config/extra/with-notcurses.mk
@@ -1,1 +1,5 @@
-LDFLAGS+=-lnotcurses -lnotcurses-core -lavutil -lavformat -lavcodec -lswresample -lavdevice -lswscale 
+LDFLAGS+=-L/usr/local/lib -lnotcurses -lnotcurses-core -lavutil -lavformat -lavcodec -lswresample -lavdevice -lswscale 
+# CFLAGS+=-DFD_DEBUG_MODE=1
+#
+CFLAGS+=-L/usr/local/lib
+LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib
